6201892128b6135c142429f3b9b8151d5d564fa1
zxhuen <zxhuen324@gmail.com>
2026-07-20 14:14:54 +0800

    added limiter

--- app/main.py
+++ app/main.py
@@ -4,7 +4,21 @@ from contextlib import asynccontextmanager
 from app.core.database import engine, Base
 from app.api.Person import router as PersonRouter
 
+from app.core.limiter import limiter
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+
 app = FastAPI(title="Backend template with alembic")
+
+app.state.limiter = limiter
+
+app.add_exception_handler(
+    RateLimitExceeded,
+    _rate_limit_exceeded_handler
+)
+
+app.add_middleware(SlowAPIMiddleware)
 
 app.include_router(PersonRouter)
 
